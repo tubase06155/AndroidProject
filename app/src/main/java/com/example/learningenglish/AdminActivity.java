b0e53d2b5faacd21d55a0ea60677a4aee7d593df
Your Namethangpvse06122 <thangpv198@gmail.com>
2019-11-05 14:43:49 +0700

    draft

--- app/src/main/java/com/example/learningenglish/AdminActivity.java
+++ app/src/main/java/com/example/learningenglish/AdminActivity.java
@@ -13,6 +13,7 @@ public class AdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+        /// ghfghjghjghjghjgjgh
         manageUser = findViewById(R.id.manageUser);
         manageLesson = findViewById(R.id.manageLesson);
         manageQuestion = findViewById(R.id.manageQuestion);
